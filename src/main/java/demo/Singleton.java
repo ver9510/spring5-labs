f8d21712ee6581a5d46b0300a8916b577f40610b
Vyacheslav-Lapin <09vUev1GPSNH4v0e>
2017-11-16 16:53:32 +0300

    dcl

---
+++ src/main/java/demo/Singleton.java
@@ -0,0 +1,18 @@
+package demo;
+
+public enum  Singleton {
+
+    private static volatile Singleton instance;
+
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null)
+            synchronized (Singleton.class) {
+                if (instance == null)
+                    instance = new Singleton();
+            }
+        return instance;
+    }
+}
