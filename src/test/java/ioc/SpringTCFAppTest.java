eaf1bc34951a68627a0dc3a832c27f600431cadd
Vera <ver9510@yandex.ru>
2017-11-23 14:54:53 +0300

    Changed formatting

--- src/test/java/ioc/SpringTCFAppTest.java
+++ src/test/java/ioc/SpringTCFAppTest.java
@@ -1,14 +1,8 @@
 package ioc;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import lab.model.Country;
 import lab.model.Person;
-import lab.model.UsualPerson;
 
 import org.hamcrest.core.Is;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +11,6 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static ioc.HelloWorldTest.getExpectedPerson;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration("classpath:ioc.xml")
