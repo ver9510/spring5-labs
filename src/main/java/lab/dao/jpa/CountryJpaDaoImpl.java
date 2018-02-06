53177a6bd78ed333851c84499dab21b1a5dde90a
Vera Ippo <ver9510@yandex.ru>
2018-02-06 17:39:08 +0300

    lab7: copied from lab7done

--- src/main/java/lab/dao/jpa/CountryJpaDaoImpl.java
+++ src/main/java/lab/dao/jpa/CountryJpaDaoImpl.java
@@ -12,46 +12,26 @@ import java.util.List;
 @Repository
 public class CountryJpaDaoImpl extends AbstractJpaDao implements CountryDao {
 
+
     @Override
     public void save(Country country) {
-//		TODO: Implement it
-        EntityManager em = emf.createEntityManager();
-        EntityTransaction transaction = em.getTransaction();
-        transaction.begin();
-        em.persist(country);
-        transaction.commit();
-        if (em != null) {
-            em.close();
-        }
+        withEntityManagerUnderTransaction(em -> em.merge(country));
     }
 
     @Override
     public List<Country> getAllCountries() {
-//	TODO: Implement it
-        List<Country> countryList = null;
-        EntityManager em = emf.createEntityManager();
-        if (em != null) {
-            countryList =
-                    em.createQuery("from Country", Country.class)
-                            .getResultList();
-            em.close();
-        }
-        return countryList;
-    }// getAllcountries()
+        return mapEntityManagerUnderTransaction(em ->
+                em.createQuery("select c from SimpleCountry c", Country.class)
+                        .getResultList());
+    }
 
     @Override
     public Country getCountryByName(String name) {
-//		TODO: Implement it
-        EntityManager em = emf.createEntityManager();
-        Country country = null;
-        if (em != null) {
-            country = em
-                    .createQuery("SELECT c FROM Country c WHERE c.name LIKE :name",
-                            Country.class).setParameter("name", name)
-                    .getSingleResult();
-            em.close();
-        }
-        return country;
+        return mapEntityManagerUnderTransaction(em ->
+                em.createQuery("select c from SimpleCountry c where c.name=:name", Country.class)
+                        .setParameter("name", name)
+                        .getSingleResult());
     }
+
 
 }
